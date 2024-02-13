cbdb3e92a087134dc00462f5abdc69a48a3f667d
Maryam Arshad <156906831+maryamarshad248@users.noreply.github.com>
2024-02-13 19:23:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict';
+// Data
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const deposits = movements.filter(function (mov, i, arr) {
+  return mov > 0;
+});
+console.log(movements);
+console.log(deposits);
+
+const depositFor = [];
+for (const mov of movements) if (mov > 0) depositFor.push(mov);
+console.log(depositFor);
+
+const withdrawls = movements.filter(mov => mov < 0);
+console.log(withdrawls);
